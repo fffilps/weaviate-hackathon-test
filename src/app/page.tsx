be5a98d0559ff606e85647f54375ea31d8a700bb
fffilps <jmariwyatt@gmail.com>
2024-09-09 16:20:48 -0700

    changed to right component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 
 // import Image from "next/image";
 // import ChatModel from "./components/ChatModel";
-// import TranscriptionForm from "./components/AssemblyAI";
+import TranscriptionForm from "./components/AssemblyAI";
 import TestText from "./components/TestText";
 
 export default function Home() {
   return (
     <div className="flex flex-col px-16">
-      {/* <TranscriptionForm /> */}
-      <TestText />
+      <TranscriptionForm />
+      {/* <TestText /> */}
       {/* <ChatModel /> */}
     </div>
   );
